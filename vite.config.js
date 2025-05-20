08c1e9aaec5278131c56cfb4eddf7303f015f245
MD Sakibur Rahman <103588995+msrofficial@users.noreply.github.com>
2025-05-20 07:00:29 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  server: {
+    port: 3000
   }
 })
